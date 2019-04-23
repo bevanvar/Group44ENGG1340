46158ba4d586c3fb8ccfd9bd3038a457471934ca
katyayanisingh <47237496+katyayanisingh@users.noreply.github.com>
2019-04-23 20:10:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct item 
+{
+	string itemname;
+	string manufacturer;
+	int quantity;
+	double price; //always set to 2 digits
+	bool instock; //true if in stock (quantity!=0), false if out of stock 
+	int numsold;
+};
+
+int main() 
+{
+	const int n = 10;
+	int *inventory = new int [n];
+	
+
+
+}
 
