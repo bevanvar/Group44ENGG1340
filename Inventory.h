832a1c7856f84f50d6dc81e6b54dcde87966d3b5
bevanvar <bevanvar@connect.hku.hk>
2019-04-27 17:40:47 +0800

    Update Inventory.h

--- Inventory.h
+++ Inventory.h
@@ -22,20 +22,20 @@ class Inventory
   public:
 					void intro();
 					void menu();
-          void swap(commodity, commodity);
-          void sortbypopularity(commodity, int);
-          void sortbyserialnumber(commodity, int);
+          void swap(commodity&, commodity&);
+          void sortbypopularity(commodity*&, int);
+          void sortbyserialnumber(commodity*&, int);
           void details(commodity);
           void warn(commodity);
-          void increasesize(commodity, int);
-          void insert(commodity, int, int, int);
-          void update(commodity, int);
-          void deletion(commodity, int);
-          void search(commodity, int);
-          void displayall(commodity, int);
-					void stockchecker(commodity, int);
-					void readinventoryfromfile(string, commodity, int);
-					void writeinventorytofile(string, commodity, int);
+          void increasesize(commodity*&, int&);
+          void insert(commodity*&, int&, int&, int&);
+          void update(commodity*&, int);
+          void deletion(commodity*&, int&);
+          void search(commodity*&, int);
+          void displayall(commodity*&, int);
+					void stockchecker(commodity*&, int);
+					void readinventoryfromfile(string, commodity*&, int&);
+					void writeinventorytofile(string, commodity*&, int);
 };
 
 
